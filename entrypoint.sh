66394cb21ed584443f08efae21fb1252e49e1160
DZorikhin <zorikhin@yandex.com>
2021-10-26 19:05:30 +0200

    add containerization

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+# The --login ensures the bash configuration is loaded,
+# enabling Conda.
+
+# Enable strict mode.
+set -euo pipefail
+
+# Temporarily disable strict mode and activate conda:
+set +euo pipefail
+conda activate ds
+
+# Re-enable strict mode:
+set -euo pipefail
+
+# exec the final command:
+exec gunicorn --bind=0.0.0.0:9696 predict:app
